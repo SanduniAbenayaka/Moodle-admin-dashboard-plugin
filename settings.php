4a45e79e3ff723f6a9f77e4b0838b3e1447379e6
Sanduni Abenayaka <43136228+SanduniAbenayaka@users.noreply.github.com>
2020-07-11 14:06:18 +0530

    Update

--- settings.php
+++ settings.php
@@ -1,13 +1,13 @@
 <?php
 $settings->add(new admin_setting_heading(
     'headerconfig',
-    get_string('headerconfig','block_myblock'),
-    get_string('descconfig','block_mybock')
+    get_string('headerconfig','block_admincourseview'),
+    get_string('descconfig','block_admincourseview')
 ));
 
 $settings->add(new admin_setting_configcheckbox(
     'myblock/Allow_HTML',
-    get_string('labelallowhtml','block_myblock'),
-    get_string('descallowhtml','block_myblock'),
+    get_string('labelallowhtml','block_admincourseview'),
+    get_string('descallowhtml','block_admincourseview'),
     '0'
 ));
