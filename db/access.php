3c9a55bc3243bdf2f7b253c43f4ad9c94021e64d
Sanduni Abenayaka <43136228+SanduniAbenayaka@users.noreply.github.com>
2020-07-11 13:50:51 +0530

    Change plugin name to admincourseview

--- db/access.php
+++ db/access.php
@@ -1,7 +1,7 @@
 <?php
     $capabilities = array(
 
-        'block/myblock:myaddinstance' => array(
+        'block/admincourseview:myaddinstance' => array(
             'captype' => 'write',
             'contextlevel' => CONTEXT_SYSTEM,
             'archetypes' => array(
@@ -11,7 +11,7 @@
             'clonepermissionsfrom'=>'moodle/my:manageblocks'
         ),
 
-        'block/myblock:addinstance'=>array(
+        'block/admincourseview:addinstance'=>array(
             'riskbitmask'=> RISK_SPAM | RISK_XSS,
 
             'captype'=>'write',
